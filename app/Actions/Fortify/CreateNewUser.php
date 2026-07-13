abbde8031d08419eb0943d3c7c6dda7267d45bc6
ahmedismail <ahmed.ismail.1627@gmail.com>
2026-07-13 05:05:52 +0300

    Add tenant and domain fields to user registration; update CreateNewUser action and User model

--- app/Actions/Fortify/CreateNewUser.php
+++ app/Actions/Fortify/CreateNewUser.php
@@ -4,6 +4,7 @@ namespace App\Actions\Fortify;
 
 use App\Concerns\PasswordValidationRules;
 use App\Concerns\ProfileValidationRules;
+use App\Models\Tenant;
 use App\Models\User;
 use Illuminate\Support\Facades\Validator;
 use Laravel\Fortify\Contracts\CreatesNewUsers;
@@ -22,12 +23,17 @@ class CreateNewUser implements CreatesNewUsers
         Validator::make($input, [
             ...$this->profileRules(),
             'password' => $this->passwordRules(),
+            'tenant' => ['required', 'string', 'max:255', 'unique:tenants,name'],
+            'domain' => ['required', 'string', 'max:255', 'unique:tenants,domain'],
         ])->validate();
+        $tenant = Tenant::create(['name' => $input['tenant'], 'domain' => $input['domain']]);
 
         return User::create([
             'name' => $input['name'],
             'email' => $input['email'],
             'password' => $input['password'],
+            'tenant_id' => $tenant->id,
+
         ]);
     }
 }
