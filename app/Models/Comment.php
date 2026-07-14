7b5fd17912126a102e9858611cd037afb044f36c
ahmedismail <ahmed.ismail.1627@gmail.com>
2026-07-14 05:20:41 +0300

    Add Comment model and migration for comments table with tenant relationship

---
+++ app/Models/Comment.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Models;
+
+use App\Concerns\BelongToTenant;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
+class Comment extends Model
+{
+    //
+    use BelongToTenant;
+
+    protected $guarded = [];
+
+    public function tenant(): BelongsTo
+    {
+        return $this->belongsTo(Tenant::class);
+    }
+}
